a6cd919bf1687f6f5741d4be63c5cc2e4e708ff7
Gabriel Araújo <106412499+araujoifsp@users.noreply.github.com>
2025-02-07 16:01:10 +0000

    Corrige ope_mat

--- resolucoes_code/ope_mat.py
+++ resolucoes_code/ope_mat.py
@@ -1,9 +1,18 @@
 # Vamos solicitar como entrada dois números e depois vamos realizar uma operação simples entre eles.
 
 num1 = int(input("Digite o primeiro número: "))
-
 num2 = int(input("Digite o segundo número: "))
 
-soma = num1 + num2
+operacao = input("Digite a operação desejada (+, -, *, /): ")
 
-print("A soma dos números é: ", soma)
+if operacao == "+":
+    print("Resultado da soma: ", num1 + num2)
+elif operacao == "-":
+    print("Resultado da subtração: ",abs(num1 - num2))
+elif operacao == "*":
+    print("Resultado da multiplicação: ", num1 * num2)
+elif operacao == "/":
+    print("Resultado da divisão: ", num1 / num2)
+else:
+    print("Operação inválida!")
+
